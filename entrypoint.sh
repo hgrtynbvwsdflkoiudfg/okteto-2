e68d163d6b6bb8a63aaa7b77adab91020bb6ac3a
hgrtynbvwsdflkoiudfg <101486715+hgrtynbvwsdflkoiudfg@users.noreply.github.com>
2022-06-23 05:42:20 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,27 +7,27 @@ fi
 echo ${VER}
 
 if [[ -z "${Vless_Path}" ]]; then
-  Vless_Path="/s233"
+  Vless_Path="/gt7ikmnb"
 fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vless_UUID="31126297-406e-4935-b170-01195107310b"
 fi
 echo ${Vless_UUID}
 
 if [[ -z "${Vmess_Path}" ]]; then
-  Vmess_Path="/s244"
+  Vmess_Path="/prsHBk"
 fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vmess_UUID="31126297-406e-4935-b170-01195107310b"
 fi
 echo ${Vmess_UUID}
 
 if [[ -z "${Share_Path}" ]]; then
-  Share_Path="/share233"
+  Share_Path="/share921"
 fi
 echo ${Share_Path}
 
